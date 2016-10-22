25423bb633a6d932b85f3b9e1ce57b68a40d1869
xuyangdong <804954666@qq.com>
2016-10-22 18:14:56 +0800

    temp

--- routes/wechathandler.js
+++ routes/wechathandler.js
@@ -1,10 +1,20 @@
 const express = require('express')
 var router = express.Router();
+const respData = `<xml>
+<ToUserName><![CDATA[toUser]]></ToUserName>
+<FromUserName><![CDATA[fromUser]]></FromUserName>
+<CreateTime>12345678</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[你好]]></Content>
+</xml>`
 
 function wechathandler(){
   return function(req,res){
     console.log('收到xml请求')
-    res.send(req.body)
+    res.set({
+      'Content-Type':'text/xml'
+    })
+    res.send(respData)
   }
 }
 
