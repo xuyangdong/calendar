9cbbea28451392a823d815a4296ff7dc0c9d367b
xuyangdong <804954666@qq.com>
2016-10-23 02:02:18 +0800

    temp

--- routes/wechathandler.js
+++ routes/wechathandler.js
@@ -4,8 +4,8 @@ const xml2js = require('xml2js');
 
 function buildXmlMsg(obj){
   return `<xml>
-  <ToUserName>${obj.ToUserName[0]}</ToUserName>
-  <FromUserName>${obj.FromUserName[0]}</FromUserName>
+  <ToUserName>${obj.FromUserName[0]}</ToUserName>
+  <FromUserName>${obj.ToUserName[0]}</FromUserName>
   <CreateTime>${Date.now()}</CreateTime>
   <MsgType>text</MsgType>
   <Content>哎呀，我的蒙，你傻了吗？</Content>
