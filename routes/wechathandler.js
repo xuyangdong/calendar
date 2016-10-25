0e6be9c0215e13bf2bd2ba5b26d83c8b022d48ba
xuyangdong <804954666@qq.com>
2016-10-25 11:51:53 +0800

    temp

--- routes/wechathandler.js
+++ routes/wechathandler.js
@@ -2,21 +2,36 @@ const express = require('express')
 const router = express.Router();
 const xml2js = require('xml2js');
 
-function buildXmlMsg(obj){
+const rule = /(),()/
+
+function buildXmlMsg(obj,content){
+
   return `<xml>
   <ToUserName>${obj.FromUserName[0]}</ToUserName>
   <FromUserName>${obj.ToUserName[0]}</FromUserName>
   <CreateTime>${Date.now()}</CreateTime>
   <MsgType>text</MsgType>
-  <Content>哎呀，我的蒙，你傻了吗？</Content>
+  <Content>${content?content:'哎呀，我的蒙，你傻了吗？'}</Content>
   </xml>`
 }
 
 function wechathandler(){
   return function(req,res){
     console.log('收到xml请求')
-    let respData = buildXmlMsg(req.body)
-    console.log("-->:",respData)
+
+    let respData;
+    const type = req.body.MsgType[0]
+    const content = req.body.Content[0]
+    switch(type){
+      case 'text':{
+
+      }
+      default :{
+        respData = buildXmlMsg(req.body)
+      }
+    }
+
+
     res.set({
       'Content-Type':'text/xml'
     })
